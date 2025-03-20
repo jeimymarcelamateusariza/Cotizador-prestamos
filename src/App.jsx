227ceacc75b426de83774e862566137414ff69b8
Jeimy Marcela Mateus Ariza <jeimymateus1@gmail.com>
2025-03-20 16:01:24 -0500

    Función calcular el pago mensual

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,20 @@ function App() {
   const [cantidad, setCantidad] = useState(10000);
   const [meses, setMeses] = useState(6);
   const [total, setTotal] = useState(0);
+  const [pago, setPago] = useState(0);
 
   useEffect(()=>{
     const resultadoTotalPagar = calcularTotalPagar(cantidad, meses);
     setTotal(resultadoTotalPagar);
+
+   
+
   }, [cantidad, meses]);
+
+  useEffect(()=>{
+     //Calcular el pago mensual
+     setPago(total/meses)
+  }, [total])
 
   const MIN = 0;
   const MAX = 20000;
@@ -87,7 +96,7 @@ function App() {
         </h2>
         <p className='text-xl text-gray-500 text-center font-bold'>{meses} Meses</p>
         <p className='text-xl text-gray-500 text-center font-bold'>{formatearDinero(total)} Total a pagar</p>
-        <p className='text-xl text-gray-500 text-center font-bold'>Mensuales</p>
+        <p className='text-xl text-gray-500 text-center font-bold'>{formatearDinero(pago)} Mensuales</p>
 
       </div>
     </div>
